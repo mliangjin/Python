cdef4f67d86820102b2668dc9b5a9048da2f4e14
mliangjin <851457309@qq.com>
2018-03-29 16:23:18 +0800

    3.29.17.23

--- Django项目/tiantiansx/df_user/urls.py
+++ Django项目/tiantiansx/df_user/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^register/', views.register)
+    url(r'^register/', views.register),
+    url(r'^register_dispose/', views.register_dispose),
+    url(r'^login/', views.login),
 ]
--- Django项目/tiantiansx/df_user/views.py
+++ Django项目/tiantiansx/df_user/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+from . import models
 
 def register(request):
     return render(request, 'df_user/register.html')
+
+def register_dispose(request):
+    name = request.POST.get('user_name')
+    pwd = request.POST.get('pwd')
+    pwd2 = request.POST.get('cpwd')
+    email = request.POST.get('email')
+    # 再判断一次密码
+    if pwd != pwd2:
+        return redirect('/user/register/')    
+    
+    # 密码加密
+    pwd3 = make_password(pwd)
+    print(pwd3)
+
+    # 保存到数据库
+    user = models.UserInfo()
+    user.name = name
+    user.pwd = pwd3
+    user.email = email
+    user.save()
+    # 重定向到登陆页面
+    return redirect('/user/login/')
+
+def login(request):
+    return render(request, 'df_user/login.html')
 
