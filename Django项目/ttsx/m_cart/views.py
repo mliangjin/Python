96c8ea72125c19be54660fdd88550b0632f8d1ae
mliangjin <851457309@qq.com>
2018-04-04 21:19:03 +0800

    4.4.22.19

--- Django项目/ttsx/m_cart/views.py
+++ Django项目/ttsx/m_cart/views.py
@@ -21,18 +21,12 @@ def add(request, goods, number):
         # 拿到用户和商品对应的表
         user = UserInfo.objects.filter(name=request.session.get('user_name'))
         # 拿到对应表中数据查找是否有对于的购物车
-        print('goods')
-        print(goods)
-        print('user[0].id')
-        print(user[0].id)
         cartinfo = CartInfo.objects.filter(goods_id=goods,user_id=user[0].id)
         # 1.判断购物车是否存在
         # 1.1 存在购物车,则继续判断购物车是否已经有商品数量
         # 1.2 不存在商品就直接添加新的商品数量
         # 1.3 否则商品数量等于现在的加原来的数量
         # 2.不存在购物车,则添加新的购物车数据
-        print('len(cartinfo)')
-        print(len(cartinfo))
         if len(cartinfo) != 0:
             if cartinfo[0].number == 0:
                 cartinfo.update(number=int(number))
